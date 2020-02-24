05dc8b215a13c8b4eff3d2ba0ea0fbd4af474f21
Bill Keese <bill@dojotoolkit.org>
2020-02-24 19:37:51 +0900

    Fixes to get IE running.

---
+++ README.md
@@ -0,0 +1,10 @@
+This is a sample of using lit-html on IE11.
+
+To run, download this repository and then do:
+
+```
+$ npm install
+$ npx webpack
+```
+
+Then load index.html on IE11.
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import "lit-html/polyfills/template_polyfill";
+// Get Object.assign().
+import "@babel/polyfill";
+
+// Get template polyfill (only needed on IE).
+import { initTemplatePolyfill } from "lit-html/polyfills/template_polyfill";
+console.log("initializing template polyfill");
+initTemplatePolyfill();
+console.log("initialized");
+
 import {html, render} from "lit-html";
 
 const h = "Hello", w = "world";
